4b89ed9b602783e6e84cea222a11fe9d4d3c5980
Constructor677 <dev-miguel-mendez@proton.me>
2026-02-09 18:42:44 -0700

    update

--- docker_scripts/run.py
+++ docker_scripts/run.py
@@ -13,6 +13,7 @@ container_name  = input(f"Enter the container name (default: {DEFAULT_CONTAINER_
 DEFAULT_ALLOWED_PUBLIC_KEY = "abc123"
 allowed_public_key = input(f"Enter the allowed public key (default: {DEFAULT_ALLOWED_PUBLIC_KEY}): ") or DEFAULT_ALLOWED_PUBLIC_KEY
 
+run_detached = input("Run detached? (y/n): ")
 
 #* Remove the container if it already exists
 try:
@@ -20,7 +21,7 @@ try:
 except subprocess.CalledProcessError:
     pass
 
-CMD = f"docker run --name {container_name}  -e ALLOWED_PUBLIC_KEY={allowed_public_key} -d {image_name}"
+CMD = f"docker run --name {container_name}  -e ALLOWED_PUBLIC_KEY={allowed_public_key} {"-d" if run_detached == "y" else ""} {image_name}"
 
 print(f"Running: \n  {CMD}")
 
